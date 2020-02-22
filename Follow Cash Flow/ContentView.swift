cefe775ffdfa686b1c5a3bd8f65ff9c7c8835b83
Антон Мушнин <anton.mushnin@gmail.com>
2020-02-22 12:14:30 +0300

    initial commit

---
+++ Follow Cash Flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Follow Cash Flow
+//
+//  Created by Антон Мушнин on 17/02/2020.
+//  Copyright © 2020 Антон Мушнин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
